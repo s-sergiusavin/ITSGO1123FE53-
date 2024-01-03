6d1d37812afec50d6bae2df906a2590ece1f734f
Eusebiu25 <eusebiub25@gmail.com>
2024-01-03 20:26:17 +0200

    salvare

---
+++ eusebiu.balas/session16/main.js
@@ -0,0 +1,50 @@
+console.log("Eusebiu");
+
+// Aceasta este o linie cu cod comentat
+
+/**
+ * acesta este un 
+ * multi line
+ * care nu v a fi luat in 
+ * considerare
+ * Data types
+ * 
+ */
+
+
+//Primitives (primitive):numbre, string, boolean, undefined, null, symbol, bigInt
+//References (referinte):objects: array, functions
+
+/**
+ * Variables declaration types
+ */
+//var- asta nu se mai foloseste
+//let- variabila dinamica, i se poate schimba valoarea
+//const- variabila constanta, nu i se poate schimba valoarea
+
+/**
+ * Number
+ */
+
+let oneNumber = 11;
+let anotherNumber = 5;
+let total;
+
+console.log(oneNumber);
+console.log(anotherNumber);
+console.log(total);
+
+
+total = oneNumber + anotherNumber;
+total = oneNumber - anotherNumber;
+total = oneNumber * anotherNumber;
+
+console.log(total);
+
+/**
+ * String
+ * (siruri de cracter0)
+ */
+
+const fullName = 'Eusebiu Balas';
+const adress = "Cluj, Romania";
