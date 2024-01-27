f990398dd9e2da8aa9cb7dfc47b08f245ac50a93
georgianamedrea <georgianamedrea83@gmail.com>
2024-01-27 18:29:08 +0200

    tema - add events in JS

---
+++ georgiana.medrea/Session_21/Social_media_app_tema/js/landingPage.js
@@ -0,0 +1,22 @@
+
+const focusInput = () => {
+    document.getElementsByTagName('label')[0].innerText = 'Enter below your first and last name';
+    document.getElementsByTagName('label')[0].style.textTransform = 'uppercase';
+    document.getElementsByTagName('input')[0].style.backgroundColor = '#ffbf00';
+    document.getElementsByTagName('input')[0].placeholder = '';
+}
+
+const blurInput = () => {
+    document.getElementsByTagName('label')[0].innerText = 'Enter your name';
+    document.getElementsByTagName('label')[0].style.textTransform = '';
+    document.getElementsByTagName('input')[0].style.backgroundColor = 'white';
+    document.getElementsByTagName('input')[0].placeholder = 'Your name';
+}
+
+function onKeyDown() {
+    document.getElementsByTagName('input')[0].style.backgroundColor = 'white';
+}
+
+function onKeyUp() {
+    document.getElementsByTagName('input')[0].style.backgroundColor = 'teal';
+}
---
+++ georgiana.medrea/Session_21/Social_media_app_tema/js/profile.js
@@ -0,0 +1,22 @@
+const addToStory = document.querySelector('.addToStory');
+
+function mouseEnterAddTo() {
+    addToStory.style.backgroundColor = 'white';
+}
+
+function mouseLeaveAddTo() {
+    addToStory.style.backgroundColor = 'lightblue';
+}
+
+const editProfile = document.getElementsByClassName('editProfile');
+
+function mouseEnterEdit() {
+    editProfile[0].style.backgroundColor = 'white';
+}
+
+function mouseLeaveEdit() {
+    editProfile[0].style.backgroundColor = 'lightblue';
+}
+
+
+
