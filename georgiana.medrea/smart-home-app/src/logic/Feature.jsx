cec887166e421f776c79a37d1b7c4405a6e204bf
georgianamedrea <georgianamedrea83@gmail.com>
2024-03-14 10:39:13 +0200

    update smart home app

--- georgiana.medrea/smart-home-app/src/logic/Feature.jsx
+++ georgiana.medrea/smart-home-app/src/logic/Feature.jsx
@@ -1,10 +1,21 @@
-import './Feature.scss';
+/**React */
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+
+/**App */
+import './Feature.scss';
 import buttonIcon from './assets/images/lights-icon.png'
 import buttonIconOff from './assets/images/lights-icon-off.png'
-import { useEffect, useState } from 'react';
+import smartHomeIcon from './assets/images/smart-home.jpg'
 
-const Feature = ({name, action, toggleAction, state}) => {
+/**Material UI */
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+const Feature = ({ name, action, toggleAction, state }) => {
 
     const [icon, setIcon] = useState('')
 
@@ -12,15 +23,36 @@ const Feature = ({name, action, toggleAction, state}) => {
         toggleAction(name)
     }
 
-    useEffect( () => {
-       state ? setIcon(buttonIcon) : setIcon(buttonIconOff)
+    useEffect(() => {
+        if (name === 'Toggle Lights') {
+            state ? setIcon(buttonIcon) : setIcon(buttonIconOff)
+        } else {
+            setIcon(smartHomeIcon);
+        }
     }, [state])
 
     return (
         <div className="feature">
-            <img src={icon} alt="" className='buttonImg'/>
-            <h3>{name}</h3>
-            <button onClick={featureButtonHandler}>{action}</button>
+            <Card sx={{ maxWidth: 250 }} onClick={featureButtonHandler}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={icon}
+                        alt="green iguana"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description to be added
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                </CardActions>
+            </Card>
         </div>
     )
 }
