1dc4135a7e008f07b61832f0e57b9472316f1dc3
vteliviu <liviu18vinturache@gmail.com>
2024-03-21 08:46:50 +0200

    lvinturache-social-media-app v.1.0.0

---
+++ liviu.vinturache/lvinturache-social-media-app/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '../pages/home/HomePage'
+import AuthPage from '../pages/auth/AuthPage'
+import NotFound from '../pages/NotFound'
+import Layout from '../components/Layout'
+
+function App() {
+
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/auth' element={<AuthPage />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
+        </Routes>
+      </Layout>
+    </>
+  )
+}
+
+export default App
