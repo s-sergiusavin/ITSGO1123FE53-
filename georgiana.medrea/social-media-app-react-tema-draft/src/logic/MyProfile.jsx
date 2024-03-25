b1bc45fddff90a55ba47ef0b0ccd37cfdac8cdc4
georgianamedrea <georgianamedrea83@gmail.com>
2024-03-25 13:26:33 +0200

    updates

--- georgiana.medrea/social-media-app-react-tema-draft/src/logic/MyProfile.jsx
+++ georgiana.medrea/social-media-app-react-tema-draft/src/logic/MyProfile.jsx
@@ -1,6 +1,15 @@
+import styles from "./MyProfile.module.scss";
+
 const MyProfile = () => {
     return (
-        <div>My profile</div>
+        <div className={styles.myProfileContainer}>
+            <header className={styles.myProfileHeader}></header>
+            <div className={styles.myProfileSections}>
+                <section className={styles.myProfileLeftSection}></section>
+                <section className={styles.myProfileRightSection}></section>
+            </div>
+
+        </div>
     )
 }
 
