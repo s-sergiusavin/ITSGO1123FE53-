878567116ae5f7252bda648b14ab6e884e5d3c78
georgianamedrea <georgianamedrea83@gmail.com>
2024-03-26 14:14:45 +0200

    last version aplicatie Sergiu

---
+++ georgiana.medrea/ssavin-social-media-app-last-version/src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import Navigation from './Navigation';
+import styles from './Layout.module.scss'
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Navigation />
+            <main className={styles.mainContent}>{children}</main>
+        </>
+    )
+}
+
+Layout.propTypes = {
+    children: PropTypes.node
+}
+
+export default Layout;
