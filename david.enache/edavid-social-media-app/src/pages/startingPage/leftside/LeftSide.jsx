0b4eb6c9415a21c890ee683929091e893b33e1a4
Enache213 <dedavid09@gmail.com>
2024-03-27 20:14:05 +0200

    work on project

--- david.enache/edavid-social-media-app/src/pages/startingPage/leftside/LeftSide.jsx
+++ david.enache/edavid-social-media-app/src/pages/startingPage/leftside/LeftSide.jsx
@@ -1,23 +1,60 @@
 import React from 'react';
 import styles from '../StartingPage.module.scss'
 import './LeftSide.scss'
+import HomeIcon from '@mui/icons-material/Home';
+import Person2Icon from '@mui/icons-material/Person2';
+import GroupIcon from '@mui/icons-material/Group';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+import CollectionsIcon from '@mui/icons-material/Collections';
+import VideoCameraFrontIcon from '@mui/icons-material/VideoCameraFront';
+import MoreIcon from '@mui/icons-material/More';
 
 const LeftSide = () => {
     return (
         <div className={styles.leftSide}>
-            <div className="sidebar">
+
+          <div className="sidebar">
       <div className="sidebar__header">
-        {/* Logo-ul sau numele aplicației */}
         <h2>My Social App</h2>
       </div>
-      <div className="sidebar__menu">
-        {/* Link-uri pentru diferitele secțiuni */}
-        <a href="#">Acasă</a>
-        <a href="#">Profil</a>
-        <a href="#">Prietenii</a>
-        <a href="#">Notificări</a>
-        <a href="#">Setări</a>
+      <div className="sidebar__menu"> 
+
+         <div className="menu__item">
+          <a href="#">Acasă</a>
+          <HomeIcon />
+        </div>
+        <div className="menu__item">
+          <a href="#">Profil</a>
+          <Person2Icon/>
+        </div>
+        <div className="menu__item">
+          <a href="#">Prieteni</a>
+          <GroupIcon/>
+        </div>
+        <div className="menu__item">
+          <a href="#">Notificari</a>
+          <NotificationsActiveIcon/>
+        </div>
+        <div className="menu__item">
+          <a href="#">Setari</a>
+          <ToggleOffIcon/>
+        </div>
+        <div className="menu__item">
+          <a href="#">Poze</a>
+          <CollectionsIcon/>
+        </div>
+        <div className="menu__item">
+          <a href="#">Videoclipuri</a>
+          <VideoCameraFrontIcon/>
+        </div>
+        <div className="menu__item">
+          <a href="#">Mai multe</a>
+          <MoreIcon/>
+        </div>
       </div>
+
+
       <div className="sidebar__footer">
         {/* Un mic footer */}
         <p>© 2024 My Social App</p>
--- david.enache/edavid-social-media-app/src/pages/startingPage/rightSide/RightSide.jsx
+++ david.enache/edavid-social-media-app/src/pages/startingPage/rightSide/RightSide.jsx
@@ -1,49 +1,73 @@
-import React from 'react';
-import styles from '../StartingPage.module.scss'
-import './RightSide.scss'
-import { useState } from 'react';
+import React, { useState } from 'react';
+import styles from '../StartingPage.module.scss';
+import './RightSide.scss';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import imagineDavid from '../images/imagineDavid.jpg';
+import imaginePostu from '../images/imaginePostu.jpg';
+import imagineDinga from '../images/imagineDinga.jpg';
+import imagineHantaru from '../images/imagineHantaru.jpg';
+import imagineFlavius from '../images/imagineFlavius.jpg';
+import imagineMariana from '../images/imagineMariana.jpg';
+import imagineCristian from '../images/imagineCristian.jpg';
+import imagineCosmin from '../images/imagineCosmin.jpg';
+import imagineVlad from '../images/imagineVlad.jpg';
+import imagineAndreea from '../images/imagineAndreea.jpg';
+import imagineLaurentiu from '../images/imagineLaurentiu.jpg';
+
+
+const friendsWithImages = [
+  { name: "David", imageUrl: imagineDavid, width: '30px', height: '30px', borderRadius: '50%'},
+  { name: "Postu", imageUrl: imaginePostu, width: '30px', height: '30px', borderRadius: '50%' },
+  { name: "Dinga", imageUrl: imagineDinga, width: '30px', height: '30px', borderRadius: '50%' },
+  { name: "Hantaru", imageUrl:  imagineHantaru, width: '30px', height: '30px', borderRadius: '50%'},
+  { name: "Flavius", imageUrl: imagineFlavius, width: '30px', height: '30px', borderRadius: '50%'},
+  { name: "Mariana", imageUrl: imagineMariana, width: '30px', height: '30px', borderRadius: '50%'},
+  { name: "Cristian", imageUrl: imagineCristian, width: '30px', height: '30px', borderRadius: '50%'},
+  { name: "Cosmin", imageUrl:  imagineCosmin, width: '30px', height: '30px', borderRadius: '50%'},
+  { name: "Vlad", imageUrl: imagineVlad, width: '30px', height: '30px', borderRadius: '50%'},
+  { name: "Andreea", imageUrl:  imagineAndreea, width: '30px', height: '30px', borderRadius: '50%'},
+  { name: "Laurentiu", imageUrl: imagineLaurentiu, width: '30px', height: '30px', borderRadius: '50%' }
+];
 
 const RightSide = () => {
-
-    const friends = ["David", "Postu", "Dinga", "Hantaru", 'Flavius'];  //sa mai pui prostule
- 
-    const [searchInput, setSearchInput] = useState("");
-
-    let friendsList = [];
+  const [searchInput, setSearchInput] = useState("");
   
-    friends.forEach((friends, index) => {
-      friendsList.push(<li key={index}>{friends}</li>);
-    }); 
- 
-     let result = [...friends];
 
-     const searchInputFn = (e) => {
-        console.log(e.target.value)
-         e.preventDefault()
-         setSearchInput(e.target.value)
-        }
-        if(searchInput.length > 2) {
+  const searchInputFn = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  }; 
 
-          result = (friends.filter((friend) => {
-            return friend.match(searchInput)
-            
-        }))
-        }
-    
-    return (
-        <div className={styles.rightSide}>
-            <div className="card">
-                <title >Friends</title>
-                <input value={searchInput} type="text" onChange={searchInputFn} placeholder='Search...' />
-                 
-                <ul>
-                    {result.map((friend, index) => {
-                        return <li key={index} >{friend}</li>
-                    })}
-                </ul>
-            </div>
-        </div>
-    );
-}
+
+  const filteredFriends = friendsWithImages.filter(friend =>
+    friend.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
+
+  return (
+    <div className={styles.rightSide}>
+      <div className="card">
+        <title>Friends</title>
+        <input value={searchInput} type="text" onChange={searchInputFn} placeholder='Search...'  style={{ marginTop: '7rem' }}/>
+       
+       <ul>
+          {filteredFriends.map((friend, index) => (
+            <li key={index}>
+              <img src={friend.imageUrl} alt={friend.name} className={styles.profileImage}
+              style={{ 
+                width: friend.width,
+                height: friend.height,
+                borderRadius: friend.borderRadius,
+              }}/>
+              <FiberManualRecordIcon className={styles.statusIcon} style={{ color: index % 2 === 0 ? 'green' : 'red' }} />
+              <span>{friend.name}</span>
+              <FiberManualRecordIcon className={styles.statusIcon} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default RightSide;
